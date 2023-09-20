2ccdfff981ddb156bf7f7e4cb12cde42c948098e
Karisson Leonardo da Silva <107573950+kaon15@users.noreply.github.com>
2023-09-20 15:33:40 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,43 @@
-function tocaSomPom (){
-document.querySelector('#som_tecla_pom').play();
+function tocaSomPom (seletorAudio) {
+  document.querySelector(seletorAudio).play();
 }
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+if (elemento && elemento.localName === 'audio' {
+    elemento.play();
+}
+  
+  else  {
+      //alert('Elemento não encontrado');
+    console.logg('Elemento não encontrado ou seletor inválido')
+ 
+  }
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+  for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1]
+    const idAudio = `#Som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom (idAudio);
+    }
+  
+    tecla.onkeydown = function (evento) {
+
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.add('ativa');
+  
+    }
+  
+  }
+
+    tecla.onkeyup = function () {
+    
+      tecla.classList.remove('ativa');
+    
+    }
+
+  }
